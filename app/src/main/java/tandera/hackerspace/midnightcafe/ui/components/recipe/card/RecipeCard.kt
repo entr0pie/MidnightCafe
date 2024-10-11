678d74faea7780a52bcfdd24d3d07b987ad023eb
tandera <tandera@notemail.com>
2024-10-10 23:00:57 -0300

    feat: add management screen structure

--- app/src/main/java/tandera/hackerspace/midnightcafe/ui/components/recipe/card/RecipeCard.kt
+++ app/src/main/java/tandera/hackerspace/midnightcafe/ui/components/recipe/card/RecipeCard.kt
@@ -1,6 +1,9 @@
 package tandera.hackerspace.midnightcafe.ui.components.recipe.card
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
@@ -17,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import tandera.hackerspace.midnightcafe.R
 import tandera.hackerspace.midnightcafe.ui.components.common.Score
 import tandera.hackerspace.midnightcafe.ui.models.RecipeModel
+import tandera.hackerspace.midnightcafe.ui.theme.Palette
 
 @Composable
 fun RecipeCard(
@@ -77,6 +82,62 @@ fun OnlyTitleRecipeCard(
         modifier = modifier,
         titleSize = titleSize
     )
+}
+
+@Composable
+fun DeletableRecipeCard(
+    recipeModel: RecipeModel,
+    modifier: Modifier = Modifier,
+    titleSize: TextUnit = TextUnit.Unspecified,
+    onDelete: () -> Unit = {},
+) {
+    DeletableRecipeCard(
+        title = recipeModel.title,
+        image = recipeModel.image,
+        imageDescription = recipeModel.imageDescription,
+        modifier = modifier,
+        titleSize = titleSize,
+        onDelete = onDelete
+    )
+}
+
+@Composable
+fun DeletableRecipeCard(
+    title: String,
+    @DrawableRes image: Int,
+    imageDescription: String,
+    modifier: Modifier = Modifier,
+    titleSize: TextUnit = TextUnit.Unspecified,
+    onDelete: () -> Unit = {},
+) {
+    Box(modifier = modifier) {
+        RecipeImage(image, imageDescription)
+        RecipeFade()
+        RecipeFooter(
+            title = title,
+            titleSize = titleSize,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        )
+
+        Box(
+            modifier = modifier
+                .background(Palette.Jet)
+                .width(50.dp)
+                .height(50.dp)
+                .align(Alignment.CenterEnd)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.trash_icon),
+                contentDescription = "",
+                modifier = modifier
+                    .width(50.dp)
+                    .padding(8.dp)
+                    .clickable { onDelete() }
+            )
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/tandera/hackerspace/midnightcafe/ui/screens/management/ManagementScreen.kt
+++ app/src/main/java/tandera/hackerspace/midnightcafe/ui/screens/management/ManagementScreen.kt
@@ -1,10 +1,74 @@
 package tandera.hackerspace.midnightcafe.ui.screens.management
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import tandera.hackerspace.midnightcafe.services.http.RECIPES
+import tandera.hackerspace.midnightcafe.ui.components.common.bars.TopBarWithArrowBack
+import tandera.hackerspace.midnightcafe.ui.components.recipe.card.DeletableRecipeCard
+import tandera.hackerspace.midnightcafe.ui.theme.Palette
 
 @Composable
 fun ManagementScreen(navController: NavController) {
-    Text(text = "Hello, world!")
+    Box(
+        modifier = Modifier
+            .background(Palette.Lavender)
+            .fillMaxSize()
+    ) {
+        Scaffold(
+            topBar = {
+                TopBarWithArrowBack(onReturnClick = {
+                    navController.popBackStack()
+                }) {
+                    Text(text = "Manage recipes")
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { }) {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
+                }
+            }
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding)
+                    .background(Palette.Lavender)
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .background(Palette.Lavender)
+                ) {
+                    items(RECIPES) { recipe ->
+                        DeletableRecipeCard(
+                            recipeModel = recipe,
+                            modifier = Modifier
+                                .height(128.dp)
+                                .padding(0.dp, 2.dp)
+                                .clickable { navController.navigate("details") },
+                            titleSize = 36.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
